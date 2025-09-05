31f8a58cf6ed18acaffed876dd56ed9c7bf3e063
1linedata <122629553+1linedata@users.noreply.github.com>
2025-09-05 13:53:05 +0700

    Add next.config.js to fix Vercel build

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Nếu bạn có dùng hình ảnh từ domain bên ngoài, hãy thêm vào đây
+  // images: {
+  //   domains: ['example.com'],
+  // },
+}
+
+module.exports = nextConfig
